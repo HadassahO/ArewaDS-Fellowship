fb56650dda9f4d83ede2a498daad3a3d8a128447
Confidence Ogozi <53949487+HadassahO@users.noreply.github.com>
2025-01-10 23:05:29 -0800

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,56 @@
+# 'Day 2: 30 Days of Python Programming'
+""" EXERCISES: LEVEL 1"""
+first_name = 'Hady'
+last_name = 'Keppy'
+full_name = "Hady Keppy"
+country = 'Nigeria'
+city = 'Lagos'
+age ='24'
+year ='2025'
+is_married = ' Married'
+is_true = True
+is_light_on = False
+
+# Declaring multiple variable on one line
+first_name,last_name,city,country,age,is_married = 'Hady','Keppy',"Lagos",'Nigeria',24, 'Married'
+
+""" EXERCISES: LEVEL 2"""
+# 1. CHECKING FOR THE DATATYPE OF THE VARIABLE ABOVE
+type(first_name)
+type(last_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_true)
+print(type(is_light_on))
+print(age)
+# 2. Find the length of your first name using len()
+len(first_name)
+
+# 3 Comparing the length of your first name and your last name
+is len(first_name) > len(last_name)
+
+# 4 Declare 5 as num_one and 4 as num_two
+num_one = 5
+num_two = 4
+
+# 5 Adding num_one and num_twom
+total = num_one + num_two
+
+# 6 Subtract num_two from num_one and assign  the value to diff
+diff = num_two - num_one
+
+# 7 Multiply num_two and num_one and assign to product
+product = num_two * num_one
+
+# 8 Divide num_one by num_two and asiign to Division
+division = num_one/num_two
+
+# 9 Modulus division to find num_two divided by num_one and assign the value to a variable remainder
+remainder = num_two % num_one
+
+# 10 Calculate num_one to the power of num_two and assign the value to a variable exp
+exp = 
